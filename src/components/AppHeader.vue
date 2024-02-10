1e2ee97abe254bbf2e7e6d2ee3fb4a3e5b346037
sarahelshark <sarahelsh183@gmail.com>
2024-02-10 18:30:52 +0100

    feat add
    header elements

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,7 +13,7 @@ export default {
     <nav>
       <ul class="main_menu">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Prodotti</a></li>
+        <li><a href="#" class="active">Prodotti</a></li>
         <li><a href="#">chi siamo</a></li>
         <li><a href="#">Contatti</a></li>
       </ul>
@@ -23,16 +23,24 @@ export default {
 
 <style scoped >
 header {
-  text-align: center;
+    background-color: var(--mol-light);
+    text-align: center;
+    padding: 2rem 0;
   & .main_menu {
+    margin-top: 2rem;
     list-style: none;
     display: flex;
     justify-content: center;
 
     & a {
       text-decoration: none;
-      padding: 0.5rem 1rem;
-      color: #333;
+      padding: 1.3rem 1rem;
+      color:var(--mol-dark);
+      font-weight: 600;
+    }
+
+    & a.active {
+        background-color: var(--mol-primary);
     }
   }
 }
