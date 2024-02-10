51cacc8b3fb430a3097ad62aa24cc1d498be0352
sarahelshark <sarahelsh183@gmail.com>
2024-02-10 18:20:07 +0100

    feat add
    COMPONENTE HEADER

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
   name: "App",
+  components:{
+    AppHeader
+  },
 };
 </script>
 
 <template>
-  <header>
-    <div class="logo">
-      <img src="/img/la-molisana-logo.png" alt="" />
-    </div>
 
-    <nav>
-      <ul class="main_menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Prodotti</a></li>
-        <li><a href="#">chi siamo</a></li>
-        <li><a href="#">Contatti</a></li>
-      </ul>
-    </nav>
-  </header>
+  <AppHeader></AppHeader>
+
   <main>main</main>
   <footer>footer</footer>
 </template>
 
 <style>
-header {
-  text-align: center;
-  & .main_menu {
-    list-style: none;
-    display: flex;
-    justify-content: center;
 
-    & a {
-      text-decoration: none;
-      padding: 0.5rem 1rem;
-      color: #333;
-    }
-  }
-}
 </style>
