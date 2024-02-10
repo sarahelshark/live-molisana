67e0a16af5135500196aaafcde10ad9104dab2fe
sarahelshark <sarahelsh183@gmail.com>
2024-02-10 19:01:26 +0100

    feat add
     footer component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
   name: "App",
   components:{
     AppHeader,
     AppMain,
+    AppFooter,
   },
-};
+}
 </script>
 
 <template>
 
   <AppHeader></AppHeader>
   <AppMain></AppMain>
-
-  
-  <footer></footer>
+  <AppFooter></AppFooter>
 </template>
 
 <style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  name: "AppFooter",
+}
+</script>
+
+<template>
+
+  <footer>footer Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo necessitatibus veniam alias omnis tempore optio, adipisci doloribus laudantium modi sint minima aspernatur beatae quibusdam sunt dignissimos perspiciatis repudiandae! Ipsa, necessitatibus hic laboriosam repudiandae repellendus, non blanditiis sapiente iure reiciendis quos omnis. Culpa asperiores animi nihil, quibusdam accusantium voluptate, sed cumque dolorum quas dignissimos fuga incidunt recusandae cupiditate nisi odio. Amet repellat vitae, ad inventore quae nisi ut enim est cupiditate facere laudantium in animi officia tempore assumenda velit qui nobis officiis quod itaque porro illum consequatur accusa
+  </footer>
+</template>
+
+<style scoped>
+
+footer {
+    background-color: var(--mol-light);
+    min-height: 400px;
+}
+</style>
