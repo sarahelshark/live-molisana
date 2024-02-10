8f9cdec385c2aac23c24a9fe12cb91681939abd6
sarahelshark <sarahelsh183@gmail.com>
2024-02-10 18:11:50 +0100

    initial commit, test componenti

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <img src="/img/la-molisana-logo.png" alt="" />
+    </div>
+
+    <nav>
+      <ul class="main_menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Prodotti</a></li>
+        <li><a href="#">chi siamo</a></li>
+        <li><a href="#">Contatti</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>main</main>
+  <footer>footer</footer>
+</template>
+
+<style>
+header {
+  text-align: center;
+  & .main_menu {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+
+    & a {
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+      color: #333;
+    }
+  }
+}
+</style>
